34a24a917a76cebd544c12c5e828954c06f1d865
Brayant17 <Brayan_tsj@hotmail.com>
2026-02-19 10:02:20 -0600

    agerga la funcionalidad del algoritmo ya creado

--- funciones.py
+++ funciones.py
@@ -1,4 +1,6 @@
 def menu():
+    total = 0
+    listaPedidos = []
     while True:
         print("Bienvenido al menú principal")
         print("1. Hamburguesa ($150)")
@@ -11,19 +13,44 @@ def menu():
         opcion = input()
         if opcion == '1':
             print("Has seleccionado Hamburguesa. Precio: $150")
-            break
+            nombrePlato = "Hamburguesa"
+            precio = 150
+            
         elif opcion == '2':
             print("Has seleccionado Pizza. Precio: $80")
-            break
+            nombrePlato = "Pizza"
+            precio = 80
+            
         elif opcion == '3':
             print("Has seleccionado Ensalada. Precio: $129")
-            break
+            nombrePlato = "Ensalada"
+            precio = 129
+            
         elif opcion == '4':
             print("Has seleccionado Tacos. Precio: $135")
-            break
+            nombrePlato = "Tacos"
+            precio = 135
+            
         elif opcion == '5':
             print("Has seleccionado Rollo de Sushi. Precio: $99")
-            break
+            nombrePlato = "Rollo de Sushi"
+            precio = 99
+            
         else:
             print("Opción no válida, por favor intente de nuevo.")
         
+        total += precio
+        listaPedidos.append(nombrePlato)
+        print(f"Total actual: ${total}")
+        print("¿Desea agregar otro platillo? (s/n): ")
+        respuesta = input().lower()
+        if respuesta != 's':
+            break
+    
+    print("--- Resumen del Pedido ---")
+    print("Resumen de tu pedido:")
+    for pedido in listaPedidos:
+        print(f"- {pedido}")
+    print(f"Total a pagar: ${total}")
+    print("Gracias por su pedido.")
+    print(" QUE ALEGRIA ")
